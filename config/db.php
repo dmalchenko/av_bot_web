8c23ec6bbb2b9d4efb7923e6261862edc9d93434
dmalchenko <malchenko.dmitriy.by@gmail.com>
2018-05-12 19:11:36 +0300

    db settings

--- config/db.php
+++ config/db.php
@@ -2,11 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host=127.0.0.1;port=5432;dbname=av',
+    'username' => 'postgres',
+    'password' => 'postgres',
     'charset' => 'utf8',
-
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
